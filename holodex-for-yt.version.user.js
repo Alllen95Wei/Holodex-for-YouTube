0e43015cd6543bb4c3287cf24475e409aa8bc055
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-05-17 23:41:33 +0800

    feat(version-js): 新增 `holodex-for-yt.version.user.js`
    用於偵測更新

---
+++ holodex-for-yt.version.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Holodex for YouTube
+// @author       Allen Wei
+// @namespace    https://github.com/Alllen95Wei
+// @match        https://www.youtube.com/*
+// @match        https://holodex.net/*
+// @version      1.0.1
+// @updateURL    https://github.com/Alllen95Wei/Holodex-for-YouTube/raw/refs/heads/main/holodex-for-yt.version.user.js
+// @downloadURL  https://github.com/Alllen95Wei/Holodex-for-YouTube/raw/refs/heads/main/holodex-for-yt.user.js
+// @grant        GM_getValues
+// @grant        GM_setValues
+// ==/UserScript==
