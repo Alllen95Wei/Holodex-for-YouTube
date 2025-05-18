f631e6da037c9cdc71c2750f222ecf1b681157bc
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-05-18 22:45:38 +0800

    feat(main-js): 升級至 v1.0.2

--- holodex-for-yt.user.js
+++ holodex-for-yt.user.js
@@ -6,7 +6,7 @@
 // @license      MIT
 // @match        https://www.youtube.com/*
 // @match        https://holodex.net/*
-// @version      1.0.1
+// @version      1.0.2
 // @updateURL    https://github.com/Alllen95Wei/Holodex-for-YouTube/raw/refs/heads/main/holodex-for-yt.version.user.js
 // @downloadURL  https://github.com/Alllen95Wei/Holodex-for-YouTube/raw/refs/heads/main/holodex-for-yt.user.js
 // @grant        GM_getValues
